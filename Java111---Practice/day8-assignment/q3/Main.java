8162fbe3cd33b4eaa47573971fa0d9687fa4966c
Nafisa Parveen <103953608+Nafisa1117@users.noreply.github.com>
2023-10-04 20:19:04 +0530

    Add files via upload

---
+++ Java111---Practice/day8-assignment/q3/Main.java
@@ -0,0 +1,16 @@
+package q3;
+
+public class Main {
+	
+	public int[] findAndReturnPrimeNumbers(int[] inputArray) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main m = new Main();
+		int[] arr = {10,12,5,50,11,14,15};
+		m.findAndReturnPrimeNumbers(arr);
+	}
+
+}
