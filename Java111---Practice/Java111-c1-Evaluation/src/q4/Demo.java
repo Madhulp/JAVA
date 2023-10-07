df4413f3bfafce46f632f7f760f2e269d016f27f
Nafisa Parveen <103953608+Nafisa1117@users.noreply.github.com>
2023-10-07 14:20:54 +0530

    q4

---
+++ Java111---Practice/Java111-c1-Evaluation/src/q4/Demo.java
@@ -0,0 +1,16 @@
+package q4;
+
+public class Demo {
+	
+	static void runScored(int a, int b, int c, int d, int e) {
+		
+		int total = a+b+c+d+e;
+		System.out.println(total);
+	}
+	
+	public static void main(String[] args) {
+		runScored(2, 4, 2, 01, 0);
+	}
+
+
+}
